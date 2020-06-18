a897f51fd836556a6b43c3061ce53ee9b57e09b5
HwDhyeon <youfsk@naver.com>
2020-06-18 11:26:13 +0900

    runner.py -> core.py 이름변경

--- timo/core.py
+++ timo/core.py
@@ -1,5 +1,5 @@
-from timo.command_manager.test_runner import TestRunner
-from timo.file_manager.config_reader import ConfigReader
+from command_manager.test_runner import TestRunner
+from file_manager.config_reader import ConfigReader
 from typing import NoReturn
 import fire
 
@@ -7,7 +7,7 @@ import fire
 class Main(object):
     def __init__(self):
         self.conf = ConfigReader('foo')
-    def run(self, test_name: str) -> NoReturn:
+    def run(self, test_name=None) -> NoReturn:
         # test_kinds = ['CSW', 'Unittest', 'Coverage', 'APItest', 'E2Etest']
         print(f'Running {test_name} in now.')
 
@@ -16,7 +16,13 @@ class AfterTest(object):
     pass
 
 
+class Pipeline(object):
+    def __init__(self):
+        self.test = Main()
+
+    def run(self):
+        self.test.run()
+
+
 if __name__ == "__main__":
-    fire.Fire({
-        'run test': print('SS')
-    })
+    fire.Fire(Pipeline)
