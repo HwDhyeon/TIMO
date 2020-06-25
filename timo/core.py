d6397b05f4282c82b6c8cf643afe21d807798009
HwDhyeon <youfsk@naver.com>
2020-06-25 13:49:19 +0900

    [추가, 수정] 간단한 명령어 추가 및 함수 이름 변경

--- timo/core.py
+++ timo/core.py
@@ -1,16 +1,21 @@
-from test_manager.test_runner import TestRunner
-from file_manager.config_reader import ConfigReader
 from database_manager.mysql import MySQL
+from file_manager.config_reader import ConfigReader
+from test_manager.test_runner import TestRunner
 from typing import NoReturn
+from utils import colored_print
 import fire
 
 
 class Main(object):
     def __init__(self):
         self.conf = ConfigReader()
-    def run(self, test_name=None) -> NoReturn:
+        self.runner = TestRunner()
+
+    def run(self, test_name) -> NoReturn:
         # test_kinds = ['CSW', 'Unittest', 'Coverage', 'APItest', 'E2Etest']
-        print(f'Running {test_name} in now.')
+        colored_print(f'Running {test_name} in now.', 'magenta')
+        command_list = self.conf.get_test_suites(test_name)
+        self.runner.run_all(command_list)
 
 
 class AfterTest(object):
@@ -20,9 +25,14 @@ class AfterTest(object):
 class Pipeline(object):
     def __init__(self):
         self.test = Main()
+        self.conf = ConfigReader()
 
-    def run(self):
-        self.test.run()
+    def run(self, test_name):
+        self.test.run(test_name)
+
+    def setting(self, ext):
+        self.conf.read_config_file(ext)
+
 
 
 if __name__ == "__main__":
