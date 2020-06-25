625e0f60e84e0c016b89f5e811ac06c4792ec5e1
HwDhyeon <youfsk@naver.com>
2020-06-25 13:51:18 +0900

    [추가] 기본적인 파일 쓰기 기능 추가

--- timo/file_manager/file_writer.py
+++ timo/file_manager/file_writer.py
@@ -1,3 +1,4 @@
+from utils import equals
 from typing import Any
 import json
 import yaml
@@ -5,5 +6,26 @@ import csv
 
 
 class Writer(object):
+
     def write_file(self, path: str, content: Any, ext: str) -> bool:
-        pass
+
+        def _save(data: Any, *, ext=''):
+            with open(file=path, mode='w', encoding='utf-8') as f:
+                if equals(ext, 'csv'):
+                    wr = csv.writer(f)
+                    wr.writerows(data)
+                else:
+                    f.write(data)
+
+        try:
+            if equals(ext, 'json'):
+                _save(json.dumps(content, indent='    '))
+            elif equals(ext, 'yaml') or equals(ext, 'yml'):
+                _save(yaml.dump(content, indent=4))
+            else:
+                _save(content, ext='csv')
+        except Exception as e:
+            print(e)
+            return False
+        else:
+            return True
