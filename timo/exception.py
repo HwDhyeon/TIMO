ad40fd42f8d8897d7d60f95130855150ba99d2a3
HwDhyeon <youfsk@naver.com>
2020-06-29 16:38:54 +0900

    [추가] UnknownTestTestToolError

--- timo/exception.py
+++ timo/exception.py
@@ -11,5 +11,13 @@ class Error(Exception):
 
 class FileExtensionError(Error):
     """This error occurs when the file extension does not meet the requirements."""
+
     def __str__(self) -> str:
         return '파일 확장명이 올바르지 않습니다.'
+
+
+class UnknownTestTestToolError(Error):
+    """This error is caused when an invalid test tool is entered."""
+
+    def __str__(self) -> str:
+        return '현재 지원하지 않거나 잘못된 테스트 도구입니다.'
