6f4d6edcd536623f66dbe9a37d8f4c55be6e2a24
HwDhyeon <youfsk@naver.com>
2020-07-06 17:05:18 +0900

    [추가, 변경] - if 비교 equals 함수로 대체 - stderr를 stdout으로 매핑함 - stdout 출력 부분 replace 적용하지 않음

--- timo/test_manager/command_runner.py
+++ timo/test_manager/command_runner.py
@@ -1,5 +1,6 @@
 from decorators import timer
 from utils import colored_print
+from utils import equals
 from utils import get_command_black_list
 from typing import List
 from typing import NoReturn
@@ -23,10 +24,11 @@ class CommandRunner(object):
         if command in get_command_black_list():  # 입력받은 커맨드가 실행하면 안되는 커맨드인지 체크
             colored_print('Out: Not supports', 'red')
             return  # 실행하면 안되는 커맨드라면 실행하지 않고 함수 종료
-        popen = subprocess.Popen(args=shlex.split(command), shell=False, stderr=subprocess.PIPE, stdout=subprocess.PIPE)  # 커맨드를 실행함
+        popen = subprocess.Popen(args=shlex.split(command), shell=False, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)  # 커맨드를 실행함
         stdout, _ = popen.communicate()  # stderr 메세지는 무시한다
         colorname = 'green'
-        if (result := stdout.decode('utf-8').replace('\n', '').replace('\r', '')) == '':  # 출력값이 아무것도 없는 경우에는 'None'으로 변경한다
+        result = stdout.decode('utf-8')
+        if result.replace('\n', '').replace('\r', '') == '':  # 출력값이 아무것도 없는 경우에는 'None'으로 변경한다
             result = 'None'
             colorname = 'red'
         colored_print(f'Out: {result}', colorname)  # 커맨드 실행결과 출력
@@ -39,7 +41,7 @@ class CommandRunner(object):
                 command_list(str): List of commands to be executed.
         """
 
-        if len(command_list) == 0:
+        if equals(len(command_list), 0):
             colored_print('No command found.', 'orage')
         else:
             for command in command_list:
