972af6c0e08fe50794b317f0164357b63dfe08f5
HwDhyeon <youfsk@naver.com>
2020-07-06 17:03:13 +0900

    [추가] Error 클래스 docstring 추가 및 두 가지 예외 추가

--- timo/exception.py
+++ timo/exception.py
@@ -3,6 +3,11 @@ from typing import NoReturn
 
 
 class Error(Exception):
+    """
+    This is the basis of all errors.\n
+    Basically the same as Exception.
+    """
+
     def __init__(self, msg: AnyStr) -> NoReturn:
         self.msg: str = msg
 
@@ -22,3 +27,17 @@ class UnknownTestTestToolError(Error):
 
     def __str__(self) -> str:
         return '현재 지원하지 않거나 잘못된 테스트 도구입니다.'
+
+
+class UnknownDataError(Error):
+    """This error occurs when data cannot be found."""
+
+    def __str__(self) -> str:
+        return '찾을 수 없거나 잘못된 데이터입니다.'
+
+
+class UnknownDatabaseError(Error):
+    """This error occurs when the database cannot be found."""
+
+    def __str__(self) -> str:
+        return '찾을 수 없거나 존재하지 않는 데이터베이스입니다.'
