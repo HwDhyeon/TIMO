fc6e94576898912e2725105619a815b69fd9d543
HwDhyeon <youfsk@naver.com>
2020-07-08 16:55:06 +0900

    [추가] Selenium 테스트 결과 파싱 지원

---
+++ timo/test_manager/tools/selenium.py
@@ -0,0 +1,26 @@
+from file_manager.file_reader import Reader
+from utils import colored_print
+
+
+class SeleniumParser(object):
+    def __init__(self):
+        self.reader = Reader()
+
+    def parse(self, path: str, file_type: str) -> dict:
+        return_val: dict = {
+            'success': 0,
+            'fail': 0,
+            'skip': 0
+        }
+        if file_type == 'xml':
+            selenium_data = self.reader.read_xml_file(path)
+            total = int(selenium_data['testsuites']['@tests'])
+            fail = int(selenium_data['testsuites']['@failures'])
+            success = total - fail
+            return_val['success'] = success
+            return_val['fail'] = fail
+        else:
+            colored_print('Sorry, there is no information you can get from this type of file.', 'orange')
+
+        return return_val
+
