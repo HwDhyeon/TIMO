3c8236c70aeb8db8ec5eb9dab9e73424b0ae6c00
HwDhyeon <youfsk@naver.com>
2020-07-09 17:02:19 +0900

    [추가] Oracle 기본 골격 생성

--- timo/database_manager/databases/oracle.py
+++ timo/database_manager/databases/oracle.py
@@ -1,5 +1,40 @@
+from decorators import timer
+from file_manager.file_reader import Reader
+from typing import NoReturn
+from utils import colored_print
 import cx_Oracle
+import os
 
 
 class Oracle(object):
-    pass
+    def __init__(self):
+        self.reader = Reader()
+
+    @timer
+    def open_DB_session(self) -> NoReturn:
+        def _read_DB_info() -> dict:
+            return self.reader.read_json_file( os.getcwd() + '/data/db.json')
+
+        try:
+            colored_print('Connecting DB...', 'yellow')
+            self.db_info = _read_DB_info()['oracle']
+            
+            db_host = self.db_info['host'] + '@' + self.db_info['port'] + '/' + self.db_info['sid']
+            self.conn = cx_Oracle.connect(
+                self.db_info['user'],
+                self.db_info['password'],
+                db_host
+            )
+            self.cursor = self.conn.cursor()
+        except Exception as e:
+            colored_print(e, 'red')
+        else:
+            colored_print('Done', 'green')
+
+    def close_DB_session(self) -> NoReturn:
+        try:
+            self.conn.commit()
+            self.conn.close()
+            self.cursor.close()
+        except Exception as e:
+            print(e, 'red')
