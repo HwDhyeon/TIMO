18b7f48ec772cd4e639683214b2b70b2d00f0219
HwDhyeon <youfsk@naver.com>
2020-07-10 15:17:36 +0900

    [추가] 런타임 환경 세팅

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -i
+#!/bin/sh
 
-echo "export PYTHONPATH=$(pwd)/timo;${PYTHONPATH}" >> "~/.bashrc"
-source "~/.bashrc"
+# PYTHONPATH에 timo 추가
+echo "export PYTHONPATH=$(pwd)/timo:$PYTHONPATH" >> ~/.bashrc
+
+# timo를 명령어로 실행할 수 있게 경로 추가
+sudo echo -e '#!/bin/bash\npython ${PYTHONPATH}/core.py "$@"' > /usr/bin/timo
+chmod +x /usr/bin/timo
