639a121b618a7a76e176df8f74a1adfa619bad0d
HwDhyeon <youfsk@naver.com>
2020-07-13 09:34:04 +0900

    [추가] MySQL 유닛테스트 스테이지 추가

--- timo/tests/test_database/test_mysql.py
+++ timo/tests/test_database/test_mysql.py
@@ -1,7 +1,9 @@
 from database_manager.databases.mysql import MySQL
+import HtmlTestRunner
 import unittest
 import json
 import os
+import yaml
 
 
 class TestMySQL(unittest.TestCase):
@@ -22,7 +24,26 @@ class TestMySQL(unittest.TestCase):
             data = json.load(f)
         assert len(data) == 1
         assert type(data[0]['MAX(BUILD_NO)']) is int
+        os.remove(os.getcwd() + '/out.json')
+
+    def test_select_query_save_yaml(self):
+        self.db.send_query(sql="SELECT MAX(BUILD_NO) FROM JENKINS_BUILD_RESULT WHERE PROJECT_NM = 'IRIS-E2E-SAAS'", type='select', save='yaml')
+        assert os.path.isfile(os.getcwd() + '/out.yaml')
+        with open(file=os.getcwd() + '/out.yaml', mode='r', encoding='utf-8') as f:
+            data = yaml.safe_load(f)
+        assert len(data) == 1
+        assert type(data[0]['MAX(BUILD_NO)']) is int
+        os.remove(os.getcwd() + '/out.yaml')
+
+    def test_select_query_save_yml(self):
+        self.db.send_query(sql="SELECT MAX(BUILD_NO) FROM JENKINS_BUILD_RESULT WHERE PROJECT_NM = 'IRIS-E2E-SAAS'", type='select', save='yml')
+        assert os.path.isfile(os.getcwd() + '/out.yml')
+        with open(file=os.getcwd() + '/out.yml', mode='r', encoding='utf-8') as f:
+            data = yaml.safe_load(f)
+        assert len(data) == 1
+        assert type(data[0]['MAX(BUILD_NO)']) is int
+        os.remove(os.getcwd() + '/out.yml')
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='./'))
