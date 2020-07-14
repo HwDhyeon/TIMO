c5e519f3b1399f7abd1d26ac5851eeb8b61a5155
HwDhyeon <youfsk@naver.com>
2020-07-14 14:52:57 +0900

    [수정] setup_env.sh이 PYTHONPATH를 설정하게 수정

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+export PYTHONPATH=$(pwd)/timo
+
 # 파이썬 의존성 패키지 설치
 pip3 install -r requirements.txt
 
 # PYTHONPATH에 timo 추가
-echo "export PYTHONPATH=$(pwd)/timo:$PYTHONPATH" >> ~/.bashrc
+echo "export PYTHONPATH=$(pwd)/timo" >> ~/.bashrc
 
 # timo를 명령어로 실행할 수 있게 경로 추가
 sudo echo -e '#!/bin/bash\npython ${PYTHONPATH}/core.py "$@"' > /usr/bin/timo
