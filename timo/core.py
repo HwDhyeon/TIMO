5aaea864ab79cb8998b1e56032540ae677ef497c
HwDhyeon <youfsk@naver.com>
2020-07-21 17:46:41 +0900

    [추가] Core
    - Docstring 추가
      - 모든 함수에 Docstring이 추가되었습니다.
    
    - 인코딩 명시
      - 이 파일이 utf-8으로 작성되었음이 명시되었습니다.

--- timo/core.py
+++ timo/core.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from colors import color
 from database_manager.database import DatabaseManager
 from file_manager.config_reader import ConfigReader
@@ -17,6 +19,13 @@ class Main(object):
         self.runner = CommandRunner()
 
     def run(self, test_name: str) -> NoReturn:
+        """
+        Execute the command.
+
+            Parameters:
+                test_name(str): Name of the test to run.
+        """
+
         # test_kinds = ['CSW', 'Unittest', 'Coverage', 'APItest', 'E2Etest']
         colored_print(f'Running {test_name} in now. 🚀', 'magenta')
         command_list = self.conf.get_test_suites(test_name)
@@ -31,6 +40,15 @@ class AfterTest(object):
         self.score = ScoreCalculator()
 
     def parse(self, test_name: str, db: str, build_number: int) -> NoReturn:
+        """
+        Parse test results.
+
+            Parameters:
+                test_name(str): The name of the test for which you want to parse the results.
+                db(str): This is the type of database if you want to store the results in a database.
+                build_number(int): Jenkins build number if you want to store the results in a database.
+        """
+
         report_conf = self.config.get_report_info(test_name=test_name)
         test_tool = self.config.get_test_tool(test_name=test_name)
         test_result = self.parser.parse(kind=test_name, file_type=report_conf['type'], test_tool=test_tool['uses'])
@@ -52,21 +70,55 @@ class Pipeline(object):
         self.after_test = AfterTest()
 
     def setting(self, ext: str) -> NoReturn:
+        """
+        Set the configuration file to be used.
+
+            Parameters:
+                ext(str): File extension.
+                          Supported forms are available at: https://github.com/HwDhyeon/TIMO#setting
+        """
+
         self.conf.read_config_file(ext)
 
-    def get(self, kind: str) -> Union[str, NoReturn]:
-        if (kind := kind.lower()) == 'name':
+    def get(self, option: str) -> Union[str, NoReturn]:
+        """
+        This is used when you want to see various information in the configuration file or the score of the entire test.
+
+            Parameters:
+                option(str): This is the information you want to view.
+
+            Returns:
+                Union(str, NoReturn): It can return a string or nothing.
+        """
+
+        if (option := option.lower()) == 'name':
             return "Project name: " + color(self.conf.get_project_name(), 'green')
-        elif kind == 'version':
+        elif option == 'version':
             return ("Project version: %s\nTIMO version: %s"
                     % (color(self.conf.get_project_version(), 'green'), color('alpha', 'orange')))
-        elif kind == 'score':
+        elif option == 'score':
             self.after_test.score.calculate()
 
     def run(self, test_name: str) -> NoReturn:
+        """
+        Perform tests.
+
+            Parameters:
+                test_name(str): Name of the test to run.
+        """
+
         self.test.run(test_name)
 
     def parse(self, test_name: str, db=None, build_number=None) -> NoReturn:
+        """
+        Parse test results.
+
+            Parameters:
+                test_name(str): The name of the test for which you want to parse the results.
+                db(str): This is the type of database if you want to store the results in a database.
+                build_number(int): Jenkins build number if you want to store the results in a database.
+        """
+
         self.after_test.parse(test_name=test_name, db=db, build_number=build_number)
 
 
