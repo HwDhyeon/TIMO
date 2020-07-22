1f24abbffdcfe569c8de365dd15b429e285c24d0
HwDhyeon <youfsk@naver.com>
2020-07-22 16:39:00 +0900

    [추가] setup_env.sh
    - 이제 data 폴더가 없으면 생성합니다.
    - 이제 conf.json 파일이 없으면 샘플 파일을 생성합니다.

--- setup_env.sh
+++ setup_env.sh
@@ -11,3 +11,12 @@ echo "export PYTHONPATH=$(pwd)/timo" >> ~/.bashrc
 # timo를 명령어로 실행할 수 있게 경로 추가
 sudo echo -e '#!/bin/bash\npython ${PYTHONPATH}/core.py "$@"' > /usr/bin/timo
 chmod +x /usr/bin/timo
+
+if ![ -d "$(pwd)/data" ]; then
+    mkdir "$(pwd)/data"
+fi
+
+if ![ -f "$(pwd)/data/conf.json" ]; then
+    cp ${PYTHONPATH}/../data/conf_template.json "$(pwd)/data/conf.json"
+
+fi
