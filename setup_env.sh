697354850a2bf0711d6689b38a8457b3052e51be
HwDhyeon <youfsk@naver.com>
2020-07-24 09:51:12 +0900

    [수정] PYTHONPATH 변경

--- setup_env.sh
+++ setup_env.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-export PYTHONPATH=$(pwd)/timo
+SCRIPT_PATH=$( cd "$(dirname "$0")" ; pwd )
+export TIMO_HOME=${SCRIPT_PATH}
+export PYTHONPATH=${TIMO_HOME}
 
 # 파이썬 의존성 패키지 설치
 pip3 install -r requirements.txt
 
 # PYTHONPATH에 timo 추가
-echo "export PYTHONPATH=$(pwd)/timo" >> ~/.bashrc
+echo "export TIMO_HOME=${TIMO_HOME}" >> ~/.bashrc
+echo "export PYTHONPATH=${TIMO_HOME}" >> ~/.bashrc
 
 # timo를 명령어로 실행할 수 있게 경로 추가
-sudo echo -e '#!/bin/bash\npython ${PYTHONPATH}/core.py "$@"' > /usr/bin/timo
+sudo echo -e '#!/bin/bash\npython ${PYTHONPATH}/timo/core.py "$@"' > /usr/bin/timo
 chmod +x /usr/bin/timo
 
 if ![ -d "$(pwd)/data" ]; then
@@ -18,5 +21,4 @@ fi
 
 if ![ -f "$(pwd)/data/conf.json" ]; then
     cp ${PYTHONPATH}/../data/conf_template.json "$(pwd)/data/conf.json"
-
 fi
