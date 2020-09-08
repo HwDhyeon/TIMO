79051cdce4539bc62d01b19e98b4fce6a3f02fae
Minsoo <zlemzlem5656@naver.com>
2020-09-08 15:35:49 +0900

    [추가] selenium.py 다량의 xml parsing 가능하도록 기능 추가

--- timo/test_manager/tools/selenium.py
+++ timo/test_manager/tools/selenium.py
@@ -1,6 +1,8 @@
 from timo.file_manager.file_reader import Reader
 from timo.utils import colored_print
 
+import glob
+
 
 class SeleniumParser(object):
     def __init__(self):
@@ -13,13 +15,16 @@ class SeleniumParser(object):
             'skip': 0
         }
         if file_type == 'xml':
-            selenium_data = self.reader.read_xml_file(path)
-            total = int(selenium_data['testsuites']['@tests'])
-            fail = int(selenium_data['testsuites']['@failures'])
-            success = total - fail
-            return_val['success'] = success
-            return_val['fail'] = fail
+            xml_path = glob.glob(path + '/**/*.xml', recursive=True)
+            for xml_file in xml_path:
+                selenium_data = self.reader.read_xml_file(xml_file)
+                total = int(selenium_data['testsuites']['@tests'])
+                fail = int(selenium_data['testsuites']['@failures'])
+                success = total - fail
+                return_val['success'] += success
+                return_val['fail'] += fail
         else:
-            colored_print('Sorry, there is no information you can get from this type of file.', 'orange')
+            colored_print(
+                'Sorry, there is no information you can get from this type of file.', 'orange')
 
         return return_val
