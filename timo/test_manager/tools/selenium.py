190f5f803f792c1464341184ce4b642b4655126e
HwDhyeon <youfsk@naver.com>
2020-09-16 10:13:38 +0900

    [추가] selenium 이제 다수의 xml 파일을 처리할 수 있습니다.

--- timo/test_manager/tools/selenium.py
+++ timo/test_manager/tools/selenium.py
@@ -1,3 +1,4 @@
+import glob
 from timo.file_manager.file_reader import Reader
 from timo.utils import colored_print
 
@@ -13,12 +14,13 @@ class SeleniumParser(object):
             'skip': 0
         }
         if file_type == 'xml':
-            selenium_data = self.reader.read_xml_file(path)
-            total = int(selenium_data['testsuites']['@tests'])
-            fail = int(selenium_data['testsuites']['@failures'])
-            success = total - fail
-            return_val['success'] = success
-            return_val['fail'] = fail
+            for file in glob.glob(path, recursive=True):
+                selenium_data = self.reader.read_xml_file(path)
+                total = int(selenium_data['testsuites']['@tests'])
+                fail = int(selenium_data['testsuites']['@failures'])
+                success = total - fail
+                return_val['success'] += success
+                return_val['fail'] += fail
         else:
             colored_print('Sorry, there is no information you can get from this type of file.', 'orange')
 
